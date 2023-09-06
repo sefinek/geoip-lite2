cedf6324817d13cee3c1c781fcb131c1eba91ad3
Sefinek <sefinek5916@gmail.com>
2023-09-06 10:24:25 +0200

    Update

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+declare module 'geoip-lite2' {
+    export type GeoData = {
+        range: [number, number];
+        country: string;
+        region: string;
+        eu: string;
+        timezone: string;
+        city: string;
+        ll: [number, number];
+        metro: number;
+        area: number;
+    };
+
+    export function lookup(ip: string): GeoData | null;
+
+    export const pretty: (n: string | number | number[]) => string;
+    export const startWatchingDataUpdate: (callback: () => void) => void;
+    export const reloadDataSync: () => void;
+    export const reloadData: (callback: () => void) => void;
+    export const version: string;
+}
