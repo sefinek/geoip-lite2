37440c922a51375add99dc4a8c215e567649f8bd
Sefinek <92880265+sefinek24@users.noreply.github.com>
2023-10-03 22:35:29 +0200

    Fixes

--- test-unminified/one.js
+++ test-unminified/one.js
@@ -1,17 +1,16 @@
 const geoIp2 = require('../lib/main.js');
-
 const ip = '86.63.89.41';
 
+// Function
 const action = async () => {
 	const data = geoIp2.lookup(ip);
 	console.log(data);
 };
 
-
+// Interval
 setInterval(async () => {
 	await action();
 }, 2000);
 
-(async () => {
-	await action();
-})();
+// Run
+(async () => await action())();
