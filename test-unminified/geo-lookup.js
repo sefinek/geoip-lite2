67439262a6da9dc67987663eb9a8eda8aa21fb48
Sefinek <contact@sefinek.net>
2023-12-22 21:15:15 +0100

    Fixes

--- test-unminified/geo-lookup.js
+++ test-unminified/geo-lookup.js
@@ -45,11 +45,11 @@ for (let i = 0; i < n; i++) {
 
 const te = +new Date();
 
-/*
-f.forEach(function(ip) {
-	console.log("%s bw %s & %s is %s", geoIp2.pretty(ip[0]), geoIp2.pretty(ip[1].range[0]), geoIp2.pretty(ip[1].range[1]), ip[1].country);
-});
-*/
+
+// f.forEach(data => {
+// 	console.log('%s bw %s & %s is %s', geoIp2.pretty(data[0]), geoIp2.pretty(data[1].range[0]), geoIp2.pretty(data[1].range[1]), data[1].country);
+// });
+
 
 console.log('Found %d (%d/%d) ips in %dms (%s ip/s) (%sμs/ip)', n, f.length, nf.length, te - ts, (n * 1000 / (te - ts)).toFixed(3), ((te - ts) * 1000 / n).toFixed(0));
 console.log('Took %d ms to startup', t2 - t1);
